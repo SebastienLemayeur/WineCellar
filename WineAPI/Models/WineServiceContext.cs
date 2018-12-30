1923d9c272efd89b01caa03b0a7175786f7a0d9d
LemayeurSebastien <sebastien.lemayeur@student.howest.be>
2018-12-30 09:53:46 +0100

    added dbcontext (no seeding)

---
+++ WineAPI/Models/WineServiceContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace WineAPI.Models
+{
+    public class WineServiceContext : DbContext
+    {
+        public WineServiceContext(DbContextOptions<WineServiceContext> options)
+             : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Wine>()
+                .ToTable("Publisher")
+                .HasData();
+
+            modelBuilder.Entity<Chateau>()
+                .ToTable("Author")
+                .HasData();
+        }
+
+        public DbSet<Wine> Publishers { get; set; }
+        public DbSet<Chateau> Authors { get; set; }
+    }
+}
