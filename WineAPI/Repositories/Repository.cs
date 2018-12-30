44786bb2aa54e9637092660f3cfd376bbfcc5cb2
LemayeurSebastien <sebastien.lemayeur@student.howest.be>
2018-12-30 11:08:12 +0100

    Added generic repo class (not implemented)

---
+++ WineAPI/Repositories/Repository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using WineAPI.Models;
+
+namespace WineAPI.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : EntityBase
+    {
+        protected readonly WineServiceContext db;
+
+        public Repository(WineServiceContext context)
+        {
+            db = context;
+        }
+
+        public Task<T> Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetFiltered(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> ListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> ListFiltered(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WineAPI/Repositories/WineRepository.cs
+++ WineAPI/Repositories/WineRepository.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WineAPI.Models;
 
 namespace WineAPI.Repositories
 {
     public class WineRepository
     {
+
     }
 }
