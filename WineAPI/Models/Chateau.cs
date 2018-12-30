55aeb3f4ddfd2971a13974176e184376b4454440
LemayeurSebastien <sebastien.lemayeur@student.howest.be>
2018-12-30 09:02:44 +0100

    added models

---
+++ WineAPI/Models/Chateau.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WineAPI.Models
+{
+    public class Chateau
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+    }
+}
---
+++ WineAPI/Models/Wine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WineAPI.Models
+{
+    public class Wine
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        public DateTime PurchasedOn{ get; set; }
+    }
+}
