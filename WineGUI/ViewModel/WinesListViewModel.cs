40a2a33adf1e0c9477d77f7719fba84ba0ba7cf2
LemayeurSebastien <sebastien.lemayeur@student.howest.be>
2019-01-02 17:12:48 +0100

    first attempt at getting wineList in gui

---
+++ WineGUI/ViewModel/WinesListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WineGUI.Helpers;
+using WineLib.DTO;
+using WineLib.Models;
+
+namespace WineGUI.ViewModel
+{
+    class WinesListViewModel
+    {
+        private readonly string baseUri = "https://localhost:44361/api/wines";
+        public WinesListViewModel()
+        {
+            IEnumerable<WineSimple> wines = ApiHelper.GetApiResult<IEnumerable<WineSimple>>(baseUri);
+            WineList = new ObservableCollection<WineSimple>(wines);
+        }
+
+        public ObservableCollection<WineSimple> WineList { get; }
+
+
+    }
+}
