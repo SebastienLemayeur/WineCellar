16edf041fc24a46d0c036d75eacf4d6b32f1bb24
LemayeurSebastien <sebastien.lemayeur@student.howest.be>
2019-01-02 18:12:48 +0100

    Bound open command (for now just shows alert box)

--- WineGUI/ViewModel/WinesListViewModel.cs
+++ WineGUI/ViewModel/WinesListViewModel.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Prism.Commands;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using WineGUI.Helpers;
 using WineLib.DTO;
 using WineLib.Models;
@@ -13,10 +16,23 @@ namespace WineGUI.ViewModel
     class WinesListViewModel
     {
         private readonly string baseUri = "https://localhost:44361/api/wines";
+        public ICommand OpenCommand { get; }
+
         public WinesListViewModel()
         {
             IEnumerable<WineSimple> wines = ApiHelper.GetApiResult<IEnumerable<WineSimple>>(baseUri);
             WineList = new ObservableCollection<WineSimple>(wines);
+            OpenCommand = new DelegateCommand(OnOpenExecute, OnOpenCanExectute);
+        }
+
+        private bool OnOpenCanExectute()
+        {
+            return true;
+        }
+
+        private void OnOpenExecute()
+        {
+            MessageBox.Show("Hi!");
         }
 
         public ObservableCollection<WineSimple> WineList { get; }
