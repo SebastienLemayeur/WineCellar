c11c3bf9e2cf666a2cbd19cd0ab6855f794bff79
LemayeurSebastien <sebastien.lemayeur@student.howest.be>
2019-01-03 13:58:56 +0100

    Made class for evenaggregator instance

---
+++ WineGUI/Helpers/EventAggregatorSingleton.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WineGUI.Helpers
+{
+    class EventAggregatorSingleton
+    {
+        private EventAggregatorSingleton()
+        {
+        }
+        private static readonly Lazy<EventAggregator> lazy = new Lazy<EventAggregator>(() => new EventAggregator());
+        public static EventAggregator Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+    }
+}
