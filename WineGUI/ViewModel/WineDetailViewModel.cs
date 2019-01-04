fc7d5ed003e3128ff9bab58d5cacef51095f25b4
LemayeurSebastien <sebastien.lemayeur@student.howest.be>
2019-01-04 11:50:16 +0100

    Added types and producers

--- WineGUI/ViewModel/WineDetailViewModel.cs
+++ WineGUI/ViewModel/WineDetailViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Prism.Events;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,8 @@ namespace WineGUI.ViewModel
         private readonly string _baseUri = "https://localhost:44361/api";
         private IEventAggregator _eventAggregator;
 
+        public ObservableCollection<Type> TypeList { get; }
+
         public WineDetailViewModel()
         {
             _eventAggregator = EventAggregatorSingleton.Instance;
@@ -25,6 +28,12 @@ namespace WineGUI.ViewModel
         private void OnOpenWineDetailView(int wineId)
         {
             Wine = ApiHelper.GetApiResult<Wine>($"{ _baseUri}/wines/{wineId}");
+
+            IEnumerable<Producer> producers = ApiHelper.GetApiResult<IEnumerable<Producer>>($"{_baseUri}/producers");
+            ProducerList = new ObservableCollection<Producer>(producers);
+
+            IEnumerable<Type> types = ApiHelper.GetApiResult<IEnumerable<Type>>($"{_baseUri}/types");
+            TypesList = new ObservableCollection<Type>(types);
         }
 
         private Wine _wine;
@@ -38,5 +47,33 @@ namespace WineGUI.ViewModel
                 OnPropertyChanged();
             }
         }
+
+        private ObservableCollection<Producer> _producerList;
+
+        public ObservableCollection<Producer> ProducerList
+        {
+            get { return _producerList; }
+            set
+            {
+                _producerList = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+        private ObservableCollection<Type> _typesList;
+
+        public ObservableCollection<Type> TypesList
+        {
+            get { return _typesList; }
+            set
+            {
+                _typesList = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
     }
 }
