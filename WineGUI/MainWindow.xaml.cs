c43162da16201d33b598c0e84b2dad12d2459772
LemayeurSebastien <sebastien.lemayeur@student.howest.be>
2019-01-04 20:41:19 +0100

    Producer and types no longer show winedetail

--- WineGUI/MainWindow.xaml.cs
+++ WineGUI/MainWindow.xaml.cs
@@ -36,11 +36,13 @@ namespace WineGUI
         private void ProducerNavBtn_Click(object sender, RoutedEventArgs e)
         {
             ListContainer.Content = new ProducersListView();
+            DetailContainer.Content = new ProducersListView();
         }
 
         private void TypeNavBtn_Click(object sender, RoutedEventArgs e)
         {
             ListContainer.Content = new TypesListView();
+            DetailContainer.Content = new TypesListView();
         }
     }
 }
