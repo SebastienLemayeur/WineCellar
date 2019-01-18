e9c174496420b912d1df3084fb04e349389ae543
LemayeurSebastien <sebastien.lemayeur@student.howest.be>
2019-01-18 13:24:25 +0100

    Implemented type detail view

---
+++ WineGUI/ViewModel/TypeDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WineLib.Models;
+
+namespace WineGUI.ViewModel
+{
+    class TypeDetailViewModel :BaseDetailViewModel<WineType>
+    {
+    }
+}
